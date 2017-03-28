fa232fffb78bc3d6f1e1ecbefbfaba0f64463071
rafalops <aluno19267@ipt.pt>
2017-03-28 13:09:24 +0100

    Inicio Operadores

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace CalculadoraCompleta.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //inicializa valor a zero
+            ViewBag.Visor = "0";
             return View();
         }
         // POST: Home
@@ -30,8 +32,41 @@ namespace CalculadoraCompleta.Controllers
                 case "8":
                 case "9":
                     //Determinar se o visor só existe um zero
-                    if (visor.Equals(0)) visor = bt;
+                    if (visor.Equals("0")) visor = bt;
                     else visor += bt; //visor = visor + bt
+                    break;
+                case ",":
+                    if (!visor.Contains(",")) visor += ",";
+                        break;
+                case "-/+":
+                    visor = Convert.ToDouble(visor) * -1 + "";//Aspas para converter para string
+                        break;
+                case "C":
+                    visor = "0";
+                    Session["PrimeiroOperador"] = true;
+                    break;
+                case "+":
+                    if((bool)Session["PrimeiroOperador"])
+                    {
+                    //guardar valor do visor
+                    Session["operando"]=visor;
+                    //limpa o visor
+                    visor = "0";
+                    //guardar o operador
+                    Session["operador"] = bt;
+                    //marcar que o primeiro operador foi registado
+                    Session["PrimeiroOperador"] = false;
+                    }
+                    else
+                    {
+                        //se não é a primeira vez que se clica num operador 
+                        //vou utilizar os valores anteriores 
+                        switch((string)Session["operador"])
+                        {
+                           // recuperar o codigo da primeira calculadora
+                        }
+                    //guardar os novos valores 
+                    }
                     break;
             }
             //entregar os valores á VIEW
